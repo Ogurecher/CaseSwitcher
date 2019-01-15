4eeea1e5d0be007a44cbabfd8cd1c3a3e6b901b2
Dmitrii <1@1.1>
2019-01-15 13:12:01 +0300

    added UPPERCASE_SNAKE

---
+++ src/com/Peredreev/CaseSwitcher/ToUppercaseSnake.java
@@ -0,0 +1,16 @@
+package com.Peredreev.CaseSwitcher;
+
+public class ToUppercaseSnake extends SwitchCase {
+
+    protected String changeName(String nameToAlter) {
+
+        nameToAlter = removePrefix(nameToAlter);
+
+        String newNameDraft = "";
+        String[] words = nameToAlter.split("(-)|(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])");
+        for (int i = 0; i < words.length; i++) {
+            newNameDraft += words[i] + "_";
+        }
+        return newNameDraft.substring(0, newNameDraft.length() - 1).toUpperCase();
+    }
+}
